5109b99f7467ad3d159f2fffe7f7ef18b3a066bb
masteroogway.devops@gamil.com <masteroogway.devops@gamil.com>
2024-11-05 12:06:57 +0000

    remove extra .git folder

--- monlog/mimir/configs/mimir-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ADVERTISE_ADDR_REPLACE=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
-echo "IP address of eth0: $ADVERTISE_ADDR_REPLACE"
-cp /etc/mimir-template.yml /etc/mimir.yml
-sed -i "s/ADVERTISE_ADDR_REPLACE/$ADVERTISE_ADDR_REPLACE/" "/etc/mimir.yml"
-
-mimir -config.file=/etc/mimir.yml
